c14bb5a0464a02b355dcbb888f863855d4dc6d14
tankaru <muramototomoya@gmail.com>
2021-05-02 14:52:56 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+const body = document.getElementsByTagName('body')[0];
+//console.log(body);
+body.addEventListener('touchstart', function(event) {
+
+}, false);
+body.addEventListener('touchend', function(event) {
+
+}, false);
+
+
+body.addEventListener('click', function(event) {
+
+}, false);
