bf1fbcded35877d0fbd50197a39672829724ba65
tankarup <muramototomoya@gmail.com>
2021-05-05 09:56:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ for (let line of directions_lines){
 
 function update_wind_shape(){
     const elem = document.getElementById('wind_shape');
-    elem.setAttribute('rotation', `0 ${parseInt(Math.max(parseInt(wind_direction)-1, 0)*360/16)} 0`);
+    //elem.setAttribute('rotation', `0 ${parseInt(Math.max(parseInt(wind_direction)-1, 0)*360/16)} 0`);
     elem.setAttribute('scale', `${parseInt(wind_speed)} 1  1`);
 }
 
